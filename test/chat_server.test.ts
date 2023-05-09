f13a381b7ab8f7bf7fdcbd723af405215fa639b4
IntegralD-503 <hunter.clark@utexas.edu>
2023-05-09 12:26:28 -0400

    Initial commit

---
+++ test/chat_server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChatServer from '../lib/chat_server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chat_server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChatServer.ChatServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
